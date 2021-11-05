60ee03d47dffd00afd8a8a22d2a8502f5111e171
盛祺晨 <shengqichen@chengqichendeMacBook-Pro.local>
2021-11-05 16:49:25 +0800

    完成版

--- README.md
+++ README.md
@@ -1,2 +1,114 @@
 # FBDP_HW6
-莎士比亚文集每个单词在各文档中出现次数
+
+金融大数据作业6
+
+姓名：盛祺晨		学号：191220093
+
+[TOC]
+
+统计莎士比亚文集每个单词在各文档中出现次数。
+
+## 设计思路
+
+### 1.map
+
+map端进行对文件的过滤大小写、去数字、去标点、过滤<3字数的单词、去常用词的过程，并将每个分出来的单词，打上标签1，写入context。基本和作业5方法一样。
+
+其中，值得注意的是，这次为了将文件名加入，我先用getPath获取全称，并找到input（指定的输入文件夹）的位置。并获取那个txt文件的名字，然后将频率设定为Text("1")，写入context。
+
+```java
+int splitIndex = split.getPath().toString().indexOf("/input");
+keyInfo.set(curword+"#"+split.getPath().toString().substring(splitIndex+7));//keyInfo例如 hadoop#file1.txt
+valueInfo.set("1");
+context.write(keyInfo,valueInfo);
+```
+
+### 2.combine
+
+combine函数中，我将map到的“单词+url”的联合体拆分，然后让map中的valueInfo的数值加起来。让一个单词成为一个key，后面跟着一堆“URL+词频”
+
+```java
+int sum = 0;
+for (Text value : values) {
+     sum += Integer.parseInt(value.toString());//将Text转换为int
+}  //	统计次数
+int splitIndex = key.toString().indexOf("#");
+  // 重新设置value值由URL和词频组成
+info.set(key.toString().substring(splitIndex + 1) + ":" + sum);
+  // 重新设置key值为单词
+key.set(key.toString().substring(0, splitIndex));
+context.write(key, info);//key例如单词hadoop，info例如file1.txt:1
+```
+
+### 3.reduce
+
+在reduce中，我用自建类DocCount 类来存储 文件名+次数 ，用treeMap存储单词，字典序自动排序。
+
+其中，treeMap部分是作业5的延续，可以将传入其中的key自动排序。只不过将key换成了String，并用compareToIgnoreCase的字符串比较方法变成字典序。
+
+```java
+private TreeMap<String, DocCount> treeMap = new TreeMap<String,DocCount>(new Comparator<String>() {
+  	@Override
+  	public int compare(String x, String y) {
+    return x.compareToIgnoreCase(y); // 不区分大小写的字典序
+    //                return x.compareTo(y); // 区分大小写的字典序
+  	}
+});
+```
+
+DocCount的形式如下：
+
+```java
+public class DocCount{
+		HashMap<String,Integer> docTimes = new HashMap<String,Integer>();
+		DocCount(String setdoc,int setTimes){
+      	docTimes.put(setdoc,setTimes);
+    }
+    void addTimes(String Doc,int add){ // 给定Doc名字和sum，在Doc中增加sum次count
+        if (docTimes.containsKey(Doc)){
+            docTimes.put(Doc, docTimes.get(Doc)+add);
+        }
+        else {
+            docTimes.put(Doc,add);
+        }
+   	}
+    List<java.util.Map.Entry<String, Integer>> sort(){
+   			List<java.util.Map.Entry<String, Integer>> list = new ArrayList<>(docTimes.entrySet());
+        Collections.sort(list, new Comparator<java.util.Map.Entry<String, Integer>>() {
+            public int compare(java.util.Map.Entry<String, Integer> entry1, java.util.Map.Entry<String, Integer> entry2) {
+                return entry2.getValue() - entry1.getValue()
+            }
+       	});
+        return list;
+    }
+}
+```
+
+## 结果展示
+
+在IDEA环境中运行
+
+![image-20211105162937399](/Users/shengqichen/Library/Application Support/typora-user-images/image-20211105162937399.png)
+
+在hadoop伪分布式上运行。打包，传参数做法如作业5.
+
+![image-20211105161249161](/Users/shengqichen/Library/Application Support/typora-user-images/image-20211105161249161.png)
+
+用 hadoop dfs -cat ouput/part-r-00000 命令可以看出正确。
+
+![image-20211105163239319](/Users/shengqichen/Library/Application Support/typora-user-images/image-20211105163239319.png)
+
+## 问题和解决
+
+## 设计改进
+
+1.路径相对写死，因为我传入的路径是固定的。其中args要穿入input和output，args[0]被我默认为input，要跳过的punctuation.txt和stop-word-list.txt是固定在input文件夹下的子目录skip。
+
+```java
+job.addCacheFile(new Path(args[0]+"/skip/punctuation.txt").toUri());
+job.addCacheFile(new Path(args[0]+"/skip/stop-word-list.txt").toUri());
+```
+
+改进：可以用GenericOptionsParser方法，把args都设定为可以用户自定义，这样会方便普适很多。
+
+2.map中使用的是Text("1")，如果可以改为IntWritable就可以不在combine中重新将string变成int，花掉一些时间代价。
--- WordCount/src/main/java/WordCount.java
+++ WordCount/src/main/java/WordCount.java
@@ -1,90 +1,184 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-
-import java.util.Comparator;
-import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.net.URI;
+import java.util.*;
 
 import org.apache.hadoop.conf.Configuration;
-
 import org.apache.hadoop.fs.Path;
-
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
-
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.StringUtils;
+
 
 public class WordCount {
-    static class Map extends Mapper<Object, Text, Text, IntWritable> {
+    static class Map extends Mapper<Object, Text, Text, Text> {
         private Text keyInfo = new Text(); // 存储单词和URL组合
+        private Text valueInfo = new Text(); // 存储词频
+        private boolean caseSensitive = false;
         private FileSplit split; // 存储Split对象
+        private Set<String> patternsToSkip = new HashSet<String>();  // 从最终结果中删除的标点符号和多余单词的列表
+        private Set<String> punctuations = new HashSet<String>(); // 用来保存所有要过滤的标点符号 stopwords
+        protected void setup(Mapper.Context context) throws IOException, InterruptedException {
+            Configuration config = context.getConfiguration();
+            this.caseSensitive = config.getBoolean("wordcount.case.sensitive", false);
+            if (config.getBoolean("wordcount.skip.patterns", false)) {
+                // 如果系统变量 wordcount.skip.patterns 为真，
+                // 则从分布式缓存文件中获取要跳过的模式列表，并将文件的 URI 转发到 parseSkipFile 方法
+                URI[] localPaths = context.getCacheFiles();
+                puncFile(localPaths[0]);
+                parseSkipFile(localPaths[1]);
+            }
+        }
+        private void puncFile(URI patternsURI) {
+            try {
+                BufferedReader fis = new BufferedReader(new FileReader(new File(patternsURI.getPath()).getName()));
+                String pattern;
+                while ((pattern = fis.readLine()) != null) {
+                    punctuations.add(pattern);
+                }
+            } catch (IOException ioe) {
+                System.out.println("Caught exception while parsing the cached file '" + patternsURI + "' : " + StringUtils.stringifyException(ioe));
+            }
+        }
+        private void parseSkipFile(URI patternsURI) {
+            try {
+                BufferedReader fis = new BufferedReader(new FileReader(new File(patternsURI.getPath()).getName()));
+                String pattern;
+                while ((pattern = fis.readLine()) != null) {
+                    patternsToSkip.add(pattern);
+                }
+            } catch (IOException ioe) {
+                System.out.println("Caught exception while parsing the cached file '" + patternsURI + "' : " + StringUtils.stringifyException(ioe));
+            }
+        }
         // 实现map函数
         @Override
-        protected void map(Object key, Text value, Mapper<Object, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        protected void map(Object key, Text value, Mapper<Object, Text, Text, Text>.Context context)
+                throws IOException, InterruptedException {
             // 获得<key,value>对所属的FileSplit对象
             split = (FileSplit) context.getInputSplit();
-            StringTokenizer itr = new StringTokenizer(value.toString());
+            String line  = (caseSensitive) ? value.toString() : value.toString().toLowerCase();
+            for (String pattern : punctuations) { // 将数据中所有满足patternsToSkip的pattern都过滤掉, replace by ""
+                line = line.replaceAll(pattern, " ");
+            }
+            line=line.replaceAll("[0-9][0-9]*"," ");// 删除数字
+            StringTokenizer itr = new StringTokenizer(line);
             while (itr.hasMoreTokens()) {
-                // key值由单词和URL组成，如"MapReduce：file1.txt"
-                // 获取文件的完整路径
-                // keyInfo.set(itr.nextToken()+":"+split.getPath().toString());
-                // 这里为了好看，只获取文件的名称。
-                int splitIndex = split.getPath().toString().indexOf("file");
-                keyInfo.set(itr.nextToken() + "#" + split.getPath().toString().substring(splitIndex));
-                // 词频初始化为1
-                context.write(keyInfo, new IntWritable(1));
+                String curword = itr.nextToken();
+                if (patternsToSkip.contains(curword) || curword.length()<3) {
+                    continue;
+                }
+                int splitIndex = split.getPath().toString().indexOf("/input");
+                keyInfo.set(curword + "#" + split.getPath().toString().substring(splitIndex+7));
+                valueInfo.set("1");
+                context.write(keyInfo,valueInfo);
             }
         }
     }
-    static class Combine extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    static class Combine extends Reducer<Text, Text, Text, Text> {
         private Text info = new Text();
-        //定义treeMap来保持统计结果,由于treeMap是按key升序排列的,这里要人为指定Comparator以实现倒排
-        //这里先使用统计数为key，被统计的单词为value
-        private TreeMap<Integer, String> treeMap = new TreeMap<Integer, String>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer x, Integer y) {
-                return y.compareTo(x);
-            }
-        });
         // 实现reduce函数
         @Override
-        protected void reduce(Text key, Iterable<Text> values, Reducer<Text, IntWritable, Text, IntWritable>.Context context) throws IOException, InterruptedException {
-            // 统计词频,放入treeMap中
+        protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+            // key存放的是单词，sum存放的是次数
+            // 统计词频
+//            System.out.println("begin Combining");
             int sum = 0;
             for (Text value : values) {
                 sum += Integer.parseInt(value.toString());
             }
-
+            //------------------
+            int splitIndex = key.toString().indexOf("#");
+            // 重新设置value值由URL和词频组成
+            info.set(key.toString().substring(splitIndex + 1) + ":" + sum);
+            // 重新设置key值为单词
+            key.set(key.toString().substring(0, splitIndex));
+            context.write(key, info);
+            //-------------------
+//            System.out.println("ending Combining...");
         }
     }
 
     public static class Reduce extends Reducer<Text, Text, Text, Text> {
-        private Text result = new Text();
-        // 实现reduce函数,给出的都是已经排序好的，做一个连接
-        @Override
-        protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
-            // 生成文档列表
-            String fileList = new String();
-            for (Text value : values) {
-                fileList += value.toString() + ";";
+        // 用DocCount 来存储 文件名+次数 ，用treeMap存储，自动排序
+        public class DocCount{
+            HashMap<String,Integer> docTimes = new HashMap<String,Integer>();
+            DocCount(String setdoc,int setTimes){
+                docTimes.put(setdoc,setTimes);
             }
-            result.set(fileList);
-            System.out.println(key);
-            System.out.println(result);
-            context.write(key, result);
+            void addTimes(String Doc,int add){ // 给定Doc名字和sum，在Doc中增加sum次count
+                if (docTimes.containsKey(Doc)){
+                    docTimes.put(Doc, docTimes.get(Doc)+add);
+                }
+                else {
+                    docTimes.put(Doc,add);
+                }
+            }
+            List<java.util.Map.Entry<String, Integer>> sort(){
+                List<java.util.Map.Entry<String, Integer>> list = new ArrayList<>(docTimes.entrySet());
+                Collections.sort(list, new Comparator<java.util.Map.Entry<String, Integer>>() {
+                            public int compare(java.util.Map.Entry<String, Integer> entry1, java.util.Map.Entry<String, Integer> entry2) {
+                                return entry2.getValue() - entry1.getValue();
+                            }
+                        }
+                );
+                return list;
+            }
         }
 
+        //用treemap来实现排序，String 存储word，实现顺序排序，
+        private TreeMap<String, DocCount> treeMap = new TreeMap<String, DocCount>(new Comparator<String>() {
+            @Override
+            public int compare(String x, String y) {
+                return x.compareToIgnoreCase(y); // 不区分大小写的字典序
+//                return x.compareTo(y); // 区分大小写的字典序
+            }
+        });
+        // 实现reduce函数
+        @Override
+        protected void reduce(Text word, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+            for (Text value : values) {// value 例子一般为 file1.txt:1
+                int splitIndex = value.toString().indexOf(":");
+                String Doc = value.toString().substring(0, splitIndex);//保证为doc名称
+                int len = value.toString().length();
+                int sum = Integer.parseInt(value.toString().substring(splitIndex + 1, len)); //存储单个单词在某文件中的次数
+                if(treeMap.containsKey(word.toString())) {//  如果 treeMap中存有word
+                    treeMap.get(word.toString()).addTimes(Doc,sum);
+                }
+                else{ // 如果没存放，直接添加word
+                    treeMap.put(word.toString(), new DocCount(Doc, sum));
+                }
+            }
+        }
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            //将treeMap中的结果,按value-key顺序写入context中
+            for (String word : treeMap.keySet()) {
+                DocCount docNum = treeMap.get(word); //
+                List<java.util.Map.Entry<String, Integer>> ls = docNum.sort();
+                StringBuilder docValueList = new StringBuilder();
+                for (int i=0;i<ls.size();i++){
+                    docValueList.append(ls.get(i).getKey()+"#"+ls.get(i).getValue()+", ");
+                }
+                context.write(new Text(word),new Text(docValueList.toString()));
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf);
+        job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
+        job.addCacheFile(new Path(args[0]+ "/skip/punctuation.txt").toUri());
+        job.addCacheFile(new Path(args[0]+ "/skip/stop-word-list.txt").toUri());
         job.setJarByClass(WordCount.class);
         // 设置Map、Combine和Reduce处理类
         job.setMapperClass(Map.class);
@@ -97,7 +191,7 @@ public class WordCount {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         // 设置输入和输出目录
-        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileInputFormat.addInputPath(job, new Path(args[0]+"/*.txt"));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
